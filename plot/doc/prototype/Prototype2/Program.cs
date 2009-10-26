8c8e314a59c7b1141b3ccf2e8f50af34e670b9af
Rushabh Ravindra Gandhi <rugandhi@syr.edu>
2009-10-26 10:04:37 -0400

    changes to threadreceiver.cs

---
+++ plot/doc/prototype/Prototype2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Prototype2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadReceiver obj = new ThreadReceiver();
+            obj.CheckList();
+        }
+    }
+}
