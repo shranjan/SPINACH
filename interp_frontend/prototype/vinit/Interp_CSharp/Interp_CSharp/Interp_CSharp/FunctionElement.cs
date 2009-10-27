d48dad5cff653c69473ffcfb9d0cc21edb8a94b4
VINIT <vnmehta@syr.edu>
2009-10-26 20:59:51 -0400

    files uploaded

---
+++ interp_frontend/prototype/vinit/Interp_CSharp/Interp_CSharp/Interp_CSharp/FunctionElement.cs
@@ -0,0 +1,44 @@
+﻿////////////////////////////////////////////////////////////////////////
+// declerationelement.cs: declares the Accept function that takes a visitor 
+// 
+// version: 1.0
+// description: part of the SPINACH code for function defination
+
+// language: C# .Net 3.5
+////////////////////////////////////////////////////////////////////////
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+
+   public class FunctionElement: Element
+    {
+        
+        Element returntype;
+        VariableElement functionname;
+        List<Element> body;
+        List<DeclarationElement> parameters;
+
+        public override void Accept(Visitor visitor)
+        {
+            visitor.VisitFunctionElement(this);
+        }
+
+        public Element getreturntype() { return returntype; }
+        public void setreturntype(Element mreturntype) { returntype = mreturntype; }
+
+        public VariableElement getfunctionname() { return functionname; }
+        public void setfunctionname(VariableElement mfunctionname) { functionname = mfunctionname; }
+
+        public List<DeclarationElement> getParameter() { return body; }
+        public void setParameter(DeclarationElement decElem) { parameters.Add(decElem); }
+
+        public List<Element> getBody() { return body; }
+        public void setBody(Element mbody) { body.Add(mbody); }
+
+
+
+    }
+
