e1850b3c41a670a84ca6948e35ea120d013a9462
Kuldeep <kpgandhi@syr.edu>
2009-11-02 21:29:48 -0500

    Commited on behalf of VINIT,Vinit's task mereged.

---
+++ interp_frontend/src/Collabrate_For_Grades/InterFrontEnd/Interp_CSharp/DeclarationElement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+    public class DeclarationElement:Element
+    {
+        String type;
+
+        Element variable;
+        public override void Accept(Visitor visitor)
+        {
+            visitor.VisitDeclarationElement(this);
+        }
+
+        public String gettype() { return type; }
+        public void settype(String mtype) { type = mtype; }
+
+        public Element getvariable() { return variable; }
+        public void setvariable(Element mvariable) { variable = mvariable; }
+
+    }
+
