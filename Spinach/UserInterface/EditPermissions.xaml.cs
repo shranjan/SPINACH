1dd422a057b736b19e913e6c6fda2f8b0f429b40
RUTU PANDYA <rkpandya@syr.edu>
2009-11-05 20:25:21 -0500

    added references and dll

---
+++ Spinach/UserInterface/EditPermissions.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UserInterface
+{
+    /// <summary>
+    /// Interaction logic for EditPermissions.xaml
+    /// </summary>
+    public partial class EditPermissions : Window
+    {
+        public EditPermissions()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChange_Click(object sender, RoutedEventArgs e)
+        {
+            if (cbUser.Text != "")
+                Close();
+            else
+                MessageBox.Show("Please select a user");
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
