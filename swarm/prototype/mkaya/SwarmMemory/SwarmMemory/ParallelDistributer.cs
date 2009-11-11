7dc5f24498dee3b2fa3a237b0eca08914242d71e
unknown <Owner@.(none)>
2009-11-11 11:55:53 -0500

    Rar!

---
+++ swarm/prototype/mkaya/SwarmMemory/SwarmMemory/ParallelDistributer.cs
@@ -0,0 +1,37 @@
+﻿//Mehmet KAYA
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Swarm;
+namespace ParallelExecution
+{
+    class ParallelDistributer
+    {
+        
+        public void distributeParallelCode(string[] statement)
+        {
+            //1. get the number of cpu of each computer
+            //2. divide the statements into that number of subgroups
+            //3. send them to the corresponding machines.
+        }
+        public void getPortion(string portion)
+        {
+            //getPortion the portions and send them To the proper function of the core teams.
+        }
+        public void getPartialResult(string resultInfo)
+        {
+            //I need the specific instance of the Swarm memory class for that string
+            //decompose the message and call the function to fill the result table via the object of the class
+            //the result table is completed?
+        }
+
+        public void sendPartialResult(string resultInfo)
+        {
+            //fill the machines result table and send to the others
+            //the result table is complted? what now?    
+        }
+
+
+    }
+}
---
+++ swarm/prototype/mkaya/SwarmMemory/SwarmMemory/Peer.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace WPF_GUI
+{
+    class ProgramInfo
+    { }
+    class Peer
+    {
+        //connection part
+        private String mIP;
+        private String mPort;
+        private String mName;
+        private String mCPU;
+
+        //computation part
+        private Hashtable RunFlag = new Hashtable();
+        private Hashtable Program = new Hashtable();
+
+        //shared infomation part
+        private String Master;
+        private String Backup;
+        private String LastHeartBeatTime;
+        private Hashtable IPtoName=new Hashtable();
+        private Hashtable NametoIP=new Hashtable();
+        private Hashtable IPtoCPU = new Hashtable();
+        
+
+        public Peer(String ip,String port,String name,String cpu)
+        {
+            mIP = ip;
+            mPort = port;
+            mName = name;
+            mCPU = cpu;
+        }
+        public void SetIP(String str) { mIP = str; }
+        public String GetIP() { return mIP; }
+
+        public void SetPort(String str) { mPort = str; }
+        public String GetPort() { return mPort; }
+
+        public void SetName(String str) { mName = str; }
+        public String GetName() { return mName; }
+
+        public void SetCPU(String str) { mCPU = str; }
+        public String GetCPU() { return mCPU; }
+
+        public void SetFlag(String pid,String flag) 
+        {
+            RunFlag[pid] = flag;
+        }
+        public String GetFlag(String pid) 
+        {
+            if(RunFlag.Contains(pid))
+                return RunFlag[pid].ToString();
+            else
+                return "-1";
+        }
+
+        public void SetMaster(String str) { Master = str; }
+        public String GetMaster() { return Master; }
+
+        public void SetBackup(String str) { Backup = str; }
+        public String GetBackup() { return Backup; }
+
+        public int InsertPeer(String ipport, String name, String cpu)
+        {
+            IPtoName[ipport] = name;
+            NametoIP[name] = ipport;
+            IPtoCPU[ipport] = cpu;
+            return 1;
+        }
+
+        public int RemovePeer(String ipport, String name)
+        {
+            if (IPtoName.Contains(ipport) && NametoIP.Contains(name))
+            {
+                IPtoName.Remove(ipport);
+                NametoIP.Remove(name);
+                IPtoCPU.Remove(ipport);
+                return 1;
+            }
+            else
+                return 0;
+        }
+
+        public Hashtable GetIPtoName() { return IPtoName; }
+        public Hashtable GetIPtoCPU() { return IPtoCPU; }
+        public Hashtable GetNametoIP() { return NametoIP; }
+
+        public void InsertProg(String pid, ref ProgramInfo prog) 
+        {
+            Program[pid] = prog;
+        }
+        public int RemoveProg(String pid) { return 0; }
+        public ProgramInfo GetProg(String pid) { return null; }
+        public int CheckRun(String pid)
+        {
+            return 0;
+        }
+
+        public String ProgOwner(String pid) { return "owner"; }
+        public int ReadAccess(String pid) { return 0; }
+        public int WriteAccess(String pid) { return 0; }
+    }
+}
